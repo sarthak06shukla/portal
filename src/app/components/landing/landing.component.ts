802bafdd8f7d08510f8784cb1e057650effb046e
sarthak06shukla <145865030+sarthak06shukla@users.noreply.github.com>
2025-08-23 02:13:07 +0530

    new commit

--- src/app/components/landing/landing.component.ts
+++ src/app/components/landing/landing.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
-
+//new cxommit changes
 @Component({
     selector: 'app-landing',
     standalone: true,
